91ca1f4c3ace790fb00ce30fd401c9f3a83f5ec9
sshindo3114 <sshindo3114@gmail.com>
2020-02-16 13:03:54 +0900

    first commit

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Shota Shindo on 2020/02/15.
+//  Copyright © 2020 Shota Shindo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
